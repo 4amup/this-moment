6eb6b4674f1acb41a5125c74033e33cd96f07194
lixiaochun-lap <lixiaochun0129@163.com>
2020-04-29 22:27:31 +0800

    clear preload

--- preload.js
+++ preload.js
@@ -1,9 +1,2 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const test = document.getElementById('test')
-  if (test) test.innerText = 'test'
-  const counter = document.getElementById('counter')
-  if (counter) counter.innerText = 12
-})
-
