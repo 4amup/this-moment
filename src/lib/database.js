1e4e5c95f15e5f0d3297c1733462c8d8c948df82
lixiaochun <lixiaochun0129@163.com>
2020-07-04 09:56:45 +0800

    Update database.js
    调整用户文件位置

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const fs = require('fs');
+const os = require('os');
 const Common = require('./common');
 const Datastore = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
@@ -6,8 +8,14 @@ const FileSync = require('lowdb/adapters/FileSync');
 // const SETTING_PATH = new FileSync(path.join(__dirname, Common.DB_SETTING));
 // const ITEM_PATH = new FileSync(path.join(__dirname, Common.DB_ITEMS));
 
-const SETTING_PATH = new FileSync(path.join(process.cwd(), Common.DB_SETTING));
-const ITEM_PATH = new FileSync(path.join(process.cwd(), Common.DB_ITEMS));
+const userdir = path.join(os.homedir(), '/now');
+
+if (!fs.existsSync(userdir)) {
+    fs.mkdirSync(userdir);
+} 
+
+const SETTING_PATH = new FileSync(path.join(userdir, '/setting.js'));
+const ITEM_PATH = new FileSync(path.join(userdir, '/items.js'));
 
 const settingDB = Datastore(SETTING_PATH);
 const itemDB = Datastore(ITEM_PATH);
